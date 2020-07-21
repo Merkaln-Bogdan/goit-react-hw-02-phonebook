565c3dd97f28c58dc0f3759af59f6cb37842d455
IlkaiGundogun <piccasoul19@ukr.net>
2020-07-21 22:33:15 +0300

    Updates

---
+++ precache-manifest.beaae8d10848e5f8b3d7000cbd55b632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4195d5de88b76f7c7659745f49df7e93",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "b15b4111c6f6be9795cd",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.972b5ee1.chunk.css"
+  },
+  {
+    "revision": "78b681b5d2ea438bfa89",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.7050f82a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.7050f82a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b15b4111c6f6be9795cd",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.0fcbb127.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
